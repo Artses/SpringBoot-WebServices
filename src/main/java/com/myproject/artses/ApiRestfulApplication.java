75a342b3bdfebffc27d77a7877535207b2b3a19b
Arthur Eustaquio <arthurreustaquio@gmail.com>
2024-02-09 19:19:46 -0300

    Initial commit

---
+++ src/main/java/com/myproject/artses/ApiRestfulApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.artses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myproject/artses/ApiRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.artses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
