2afab2e879ed1ccc1b342cf30d0b768ba612b220
Arthur Eustaquio <arthurreustaquio@gmail.com>
2024-02-09 21:03:39 -0300

    Add H2 database,JPA, and test profile

--- src/main/java/com/myproject/artses/entities/User.java
+++ src/main/java/com/myproject/artses/entities/User.java
@@ -1,13 +1,19 @@
 package com.myproject.artses.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
